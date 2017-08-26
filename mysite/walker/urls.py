5d70271c83828557a84e790ddcc35bcfa8dcbaf7
TeddyGuo <error35643527@gmail.com>
2017-08-27 04:28:32 +0800

    One more time for html

--- mysite/walker/urls.py
+++ mysite/walker/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpattern = [
     #首頁（正規表達式 ^$）
     url(r'^$', views.home, name='home'),
-    url(r'^post/(?P<pk>\d+)/$', post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
