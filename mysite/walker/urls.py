734497c99a24e1c8af6e06decdd6cdd593d56ba0
TeddyGuo <error35643527@gmail.com>
2017-08-27 04:59:27 +0800

    Finally, I set up the Html problem

--- mysite/walker/urls.py
+++ mysite/walker/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-urlpattern = [
+urlpatterns = [
     #首頁（正規表達式 ^$）
     url(r'^$', views.home, name='home'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
--- mysite/walker/views.py
+++ mysite/walker/views.py
@@ -5,9 +5,9 @@ from .models import Post
 # walker/views.py.
 def home(request):
     post_list = Post.objects.all()
-    return render(request, '../templates/home.html',{
+    return render(request, 'home.html',{
         'post_list':post_list,
     })
 def post_detail(request, pk):
     post = Post.objects.get(pk = pk)
-    return render(request, '../templates/post.html', {'post':post})
+    return render(request, 'post.html', {'post':post})
