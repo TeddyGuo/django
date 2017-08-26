2402cee57c841c33ad687e30d579916c1d7624e0
TeddyGuo <error35643527@gmail.com>
2017-08-27 02:23:28 +0800

    My Teddyjango app, first commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,27 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+#import views function from walker app.
+from walker.views import hello_world, home, post_detail
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    #首頁（正規表達式 ^$）
+    url(r'^$',home),
+    url(r'^hello/$',hello_world),
+    url(r'^post/(?P<pk>\d+)/$', post_detail, name='post_detail'),
+]
---
+++ mysite/walker/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from datetime import datetime
+from .models import Post
+
+# walker/views.py.
+def hello_world(request):
+    return render(request, 'hello_world.html',{
+        'current_time':str(datetime.now()),
+    })
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html',{
+        'post_list':post_list,
+    })
+def post_detail(request, pk):
+    post = Post.objects.get(pk = pk)
+    return render(request, 'post.html', {'post':post})
