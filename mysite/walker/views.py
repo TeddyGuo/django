06bada21bf90029107bb087d44325f5d651366e3
TeddyGuo <error35643527@gmail.com>
2017-08-28 12:14:10 +0800

    A big change for the Html

--- mysite/walker/views.py
+++ mysite/walker/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import datetime
 from .models import Post
 
 # walker/views.py.
 def home(request):
-    post_list = Post.objects.all().order_by('created_date')
+    post_list = Post.objects.all().order_by('-created_date')
     return render(request, 'home.html',{
         'post_list':post_list
     })
 def post_detail(request, pk):
-    post = Post.objects.get(pk = pk)
+    post = get_object_or_404(Post, pk = pk)
     return render(request, 'post.html', {'post':post})
