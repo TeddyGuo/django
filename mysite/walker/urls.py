1a6d98c849393cc1cf529548ab1f43c8ca380fec
TeddyGuo <error35643527@gmail.com>
2017-08-29 14:53:11 +0800

    Finally, I got it done

--- mysite/walker/urls.py
+++ mysite/walker/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     #首頁（正規表達式 ^$）
     url(r'^$', views.home, name='home'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- mysite/walker/views.py
+++ mysite/walker/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from datetime import datetime
 from .models import Post
+from .forms import PostForm
+from django.utils import timezone
 
 # walker/views.py.
 def home(request):
@@ -11,3 +13,28 @@ def home(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, 'post.html', {'post':post})
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'post_edit.html', {'form': form})
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_edit.html', {'form': form})
